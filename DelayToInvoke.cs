3e1373f7c843d56143acf9bc6b06cc27be106155
ice-saveworld <ice_saveworld@126.com>
2017-12-03 23:39:31 +0800

    延迟执行函数_协程封装

---
+++ DelayToInvoke.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class DelayToInvoke : MonoBehaviour {
+    public static IEnumerator DelayToInvokeDo(Action action, float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        action();
+    }
+}
