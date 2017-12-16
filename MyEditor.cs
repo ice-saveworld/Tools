371009b73403ca95d7eb983369333c452046f8e5
ice-saveworld <ice_saveworld@126.com>
2017-12-16 14:57:47 +0800

    Add files via upload
    读取excel表

---
+++ MyEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Excel;
+using UnityEditor;
+using UnityEngine;
+using System.Data;
+public class MyEditor : Editor {
+    [MenuItem("Tools/MyEditor/RoleAttribute")]
+    static void RoleAttribute()
+    {
+        //Debug.Log(Application.dataPath + "/RoleAttributes.xlsx");
+        FileStream stream = File.Open(Application.dataPath + "/RoleAttributes.xlsx", FileMode.Open, FileAccess.Read);
+        IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        //do
+        //{
+        //    Debug.Log(excelReader.Name);
+        //    while (excelReader.Read())
+        //    {
+        //        for (int i = 0; i < excelReader.FieldCount; i++)
+        //        {
+        //            string value = excelReader.IsDBNull(i) ? "" : excelReader.GetString(i);
+        //            Debug.Log(value);
+        //        }
+        //    }
+        //} while (excelReader.NextResult());
+
+        DataSet result = excelReader.AsDataSet();
+        int columns = result.Tables[0].Columns.Count;
+        int rows = result.Tables[0].Rows.Count;
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                string value = result.Tables[0].Rows[i][j].ToString();
+                if (value == null || value == "") break;
+                Debug.Log(value);
+            }
+        }
+    }
+}
