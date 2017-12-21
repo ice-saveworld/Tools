e99b0c3c6c22caab152e5dc90a35d1314fd44403
ice-saveworld <ice_saveworld@126.com>
2017-12-21 20:32:21 +0800

    序列化Dictionary

---
+++ SerializableDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SerializableDictionary : Dictionary<string, Language>,ISerializationCallbackReceiver {
+    [SerializeField]private List<string> _keys=new List<string>();
+    [SerializeField]private List<Language> _values=new List<Language>();
+    public void OnBeforeSerialize()
+    {
+        _keys.Clear();
+        _values.Clear();
+        foreach (var item in this)
+        {
+            _keys.Add(item.Key);
+            _values.Add(item.Value);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+        for (int i = 0; i < Mathf.Min(_keys.Count, _values.Count); i++)
+        {
+            this.Add(_keys[i], _values[i]);
+        }
+    }
+
+    
+}
